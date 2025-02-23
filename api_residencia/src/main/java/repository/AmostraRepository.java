c33d7b6c734fe7cb7cdec5fd6e1f5c88cf53f4ec
Gabriel.Almeida-10 <105819653+Gabriel-Almeida0@users.noreply.github.com>
2025-02-22 22:22:07 -0300

    Delete api_residencia directory

--- api_residencia/src/main/java/repository/AmostraRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.AmostraRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AmostraRepository extends JpaRepository<AmostraRequest, Long> {
-}
