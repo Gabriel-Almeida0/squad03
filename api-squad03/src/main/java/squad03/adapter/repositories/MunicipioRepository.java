f26ce5b93061adbb5de070588eeefb1d76486047
Gabriel.Almeida-10 <105819653+Gabriel-Almeida0@users.noreply.github.com>
2025-02-24 15:49:16 -0300

    Delete api-squad03 directory

--- api-squad03/src/main/java/squad03/adapter/repositories/MunicipioRepository.java
+++
@@ -1,7 +0,0 @@
-package squad03.adapter.repositories;
-
-import squad03.adapter.entities.MunicipioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MunicipioRepository extends JpaRepository<MunicipioEntity, Long> {
-}
